5f064d33bc7b736a6809b66deebcd18f40bd9f39
See Yang <seeyang24@gmail.com>
2019-10-05 13:16:59 -0500

    added textarea and inputform to editmovie.js

--- src/components/EditMovie/EditMovie.js
+++ src/components/EditMovie/EditMovie.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class EditMovie extends Component {
+
+    inputChange = (event, propertyName) => {
+        console.log('inPutChange', event.target.value);
+    }
+
     render() {
         return (
             <div>
-                <p>Hello</p>
+                <div>
+                    <h2>Edit The Following:</h2>
+                    <form id="changeForm">
+                        <input onChange={(event) => {this.inputChange(event, 'title')}} placeholder="Movie Title"></input>
+                    </form>
+                    <br />
+                    <textarea onChange={(event) => {this.inputChange(event, 'description')}} rows="8" cols="50" form="changeForm"></textarea>
+                </div>
+                <button>Cancel</button>
+                <button>Save</button>
             </div>
         )
     }
