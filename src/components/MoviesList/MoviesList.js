cdbbdce7db134878a13869db4d28fcccbf369a13
See Yang <seeyang24@gmail.com>
2019-10-04 21:32:11 -0500

    aded grid styling to movieslist.js

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+// material UI imports
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing.unit * 2,
+    },
+  });
 
 class MoviesList extends Component {
   // Renders the entire app on the DOM
@@ -15,6 +35,9 @@ class MoviesList extends Component {
 
   
   render() {
+
+    const { classes } = this.props;
+
     return (
       <div className="movieItems">
         <h2>Movies to Watch</h2>
@@ -22,10 +45,19 @@ class MoviesList extends Component {
             {this.props.reduxState.setMovies.map( (movie) => {
                 return (
                 <div key={movie.id}>
-                    <Link to={`/details/${movie.id}`} className="Link">
-                    <img className="MovieImage" src={movie.poster} alt="movie"></img> 
-                    </Link>
-                    <p>{movie.title}</p>
+                    <Grid container className={classes.root} spacing= {16} justify="center">
+                        <Grid item xs={5}>
+                                <Paper>    
+                                    <Link to={`/details/${movie.id}`} className="Link">
+                                    <img className="MovieImage" src={movie.poster} alt="movie"></img> 
+                                    </Link>
+                                    <p>{movie.title}</p>
+                                    <IconButton aria-label="Add to favorites">
+                                        <FavoriteIcon />
+                                    </IconButton>
+                                </Paper>
+                        </Grid>
+                    </Grid>
                 </div>
                 )
             })}
@@ -40,4 +72,8 @@ const mapStateToProps = reduxState => ({
     reduxState,
 });
 
-export default connect(mapStateToProps) (MoviesList);
+MoviesList.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default connect(mapStateToProps) (withStyles(styles) (MoviesList));
